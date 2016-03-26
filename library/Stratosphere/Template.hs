db1aecdecb347e8607425754e5d44a738b9451b0
David Reaver <johndreaver@gmail.com>
2016-03-25 17:17:05 -0700

    Finished untyped Template type

--- library/Stratosphere/Template.hs
+++ library/Stratosphere/Template.hs
@@ -6,7 +6,9 @@
 
 module Stratosphere.Template
        ( Template (..)
+       , Parameter (..)
        , Resource (..)
+       , Output (..)
        , templateDefault
        ) where
 
@@ -16,6 +18,25 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
 import GHC.Exts (fromList)
 
+data Parameter =
+  Parameter
+  { parameterType :: T.Text
+  , parameterDefault :: Maybe Value
+  , parameterNoEcho :: Maybe Bool
+  , parameterAllowedValues :: Maybe Array
+  , parameterAllowedPattern :: Maybe T.Text
+  , parameterMaxLength :: Maybe T.Text -- Integer
+  , parameterMinLength :: Maybe T.Text -- Integer
+  , parameterMaxValue :: Maybe T.Text -- Integer
+  , parameterMinValue :: Maybe T.Text -- Integer
+  , parameterDescription :: Maybe T.Text
+  , parameterConstraintDescription :: Maybe T.Text
+  } deriving (Show)
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 9
+                            , omitNothingFields = True } ''Parameter)
+
+type Mapping = HM.HashMap T.Text Object
 
 data Resource =
   Resource
@@ -25,14 +46,24 @@ data Resource =
 
 $(deriveJSON defaultOptions { fieldLabelModifier = drop 8 } ''Resource)
 
+data Output =
+  Output
+  { outputDescription :: T.Text
+  , outputValue :: Object
+  } deriving (Show)
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 6 } ''Output)
 
 data Template =
   Template
   { templateFormatVersion :: Maybe T.Text
   , templateDescription :: Maybe T.Text
   , templateMetadata :: Maybe Object
-  , templateMappings :: Maybe (HM.HashMap T.Text (HM.HashMap T.Text Object))
+  , templateParameters :: Maybe (HM.HashMap T.Text Parameter)
+  , templateMappings :: Maybe (HM.HashMap T.Text Mapping)
+  , templateConditions :: Maybe Object
   , templateResources :: HM.HashMap T.Text Resource
+  , templateOutputs :: Maybe (HM.HashMap T.Text Output)
   } deriving (Show)
 
 $(deriveJSON defaultOptions { fieldLabelModifier = drop 8
@@ -44,6 +75,9 @@ templateDefault =
   { templateFormatVersion = Nothing
   , templateDescription = Nothing
   , templateMetadata = Nothing
+  , templateParameters = Nothing
   , templateMappings = Nothing
+  , templateConditions = Nothing
   , templateResources = fromList []
+  , templateOutputs = Nothing
   }
