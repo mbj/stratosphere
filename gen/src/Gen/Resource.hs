201bd1d4a404f6b7edf739cfc70384aa4620a62e
David Reaver <johndreaver@gmail.com>
2016-03-27 13:09:59 -0700

    Split up gen main module

---
+++ gen/src/Gen/Resource.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | Parse the resource JSON files
+
+module Gen.Resource where
+
+import Control.Lens hiding ((.=))
+import Data.Aeson
+import Data.Aeson.TH
+import qualified Data.Text as T
+import qualified Filesystem as FS
+import qualified Filesystem.Path as FP
+
+data ResourceParameterType
+  = String
+  | Boolean
+  deriving (Show)
+
+$(deriveJSON defaultOptions ''ResourceParameterType)
+
+data ResourceParameter =
+  ResourceParameter
+  { _resourceParameterName :: T.Text
+  , _resourceParameterType' :: ResourceParameterType
+  , _resourceParameterDocumentation :: T.Text
+  , _resourceParameterRequired :: Bool
+  } deriving (Show)
+
+$(deriveJSON
+  defaultOptions { fieldLabelModifier = drop 18 . filter (/= '\'')}
+  ''ResourceParameter)
+$(makeFields ''ResourceParameter)
+
+data Resource =
+  Resource
+  { _resourceName :: T.Text
+  , _resourceParameters :: [ResourceParameter]
+  } deriving (Show)
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 9 } ''Resource)
+$(makeFields ''Resource)
+
+-- | Read a resource JSON file and try to parse a Resource from it.
+parseResource :: FP.FilePath -> IO (Either String Resource)
+parseResource fp = fmap eitherDecodeStrict (FS.readFile fp)
