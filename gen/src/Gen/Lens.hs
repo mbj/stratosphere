6b234b08cd5431e6cc238485b05121b295faff4b
David Reaver <johndreaver@gmail.com>
2016-03-30 06:31:23 -0700

    Disambiguate lens names

--- gen/src/Gen/Lens.hs
+++ gen/src/Gen/Lens.hs
@@ -5,8 +5,10 @@
 module Gen.Lens where
 
 import Control.Lens
+import Data.Char (toLower)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import Data.Text.Manipulate (lowerHead)
+import Data.Text.Manipulate (toAcronym)
 
 import Gen.Resource
 import Gen.Types
@@ -17,13 +19,19 @@ renderLenses res = T.intercalate "\n\n" lenses
 
 renderLens :: T.Text -> ResourceParameter -> T.Text
 renderLens resName rp = T.intercalate "\n" [typeDecl, funcDef]
-  where lensName = lowerHead (rp ^. name)
+  where lensName' = lensName resName rp
         typeText = renderFieldType rp
         wrapType = case T.head typeText of
                      '[' -> typeText
                      '(' -> typeText
                      _   -> T.concat ["(", typeText, ")"]
-        typeDecl = T.concat [lensName, " :: Lens' ", resName, " ", wrapType]
+        typeDecl = T.concat [lensName', " :: Lens' ", resName, " ", wrapType]
         fieldName = renderFieldName resName rp
-        funcDef = T.concat [lensName, " = lens ", fieldName,
+        funcDef = T.concat [lensName', " = lens ", fieldName,
                             " (\\s a -> s { ", fieldName, " = a })"]
+
+
+lensName :: T.Text -> ResourceParameter -> T.Text
+lensName resName rp = T.append prefix' (rp ^. name)
+  where prefix = fromMaybe "" (toAcronym resName)
+        prefix' = T.pack $ toLower <$> T.unpack prefix
