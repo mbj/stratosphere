0be935df0ccca691c6a19e5f0d75b2024a49e6c2
David Reaver <johndreaver@gmail.com>
2016-04-10 12:27:47 -0700

    Better example indentation

--- examples/ec2-with-eip.hs
+++ examples/ec2-with-eip.hs
@@ -16,62 +16,62 @@ import Stratosphere
 myTemplate :: Template
 myTemplate =
   template
-    [ resource "EC2Instance" (
-        EC2InstanceProperties $
-        ec2Instance
-        "ami-22111148"
-        & eciInstanceType ?~ Ref "InstanceType"
-        & eciKeyName ?~ Ref "KeyName"
-        & eciUserData ?~ Base64 (Join "" ["IPAddress=", Ref "IPAddress"])
-        & eciSecurityGroups ?~ [Ref "InstanceSecuritygroup"]
-        )
-      & deletionPolicy ?~ Retain
-    , resource "InstanceSecurityGroup" $
-      SecurityGroupProperties $
-      securityGroup
-      "Enable SSH Access"
-      & sgSecurityGroupIngress ?~ [
-        securityGroupIngressRule
-        "tcp"
-        & sgirFromPort ?~ Literal 22
-        & sgirToPort ?~ Literal 22
-        & sgirCidrIp ?~ Ref "SSHLocation"
-        ]
-    , resource "IPAddress" (EIPProperties eip)
-    , resource "IPAssoc" $
-      EIPAssociationProperties $
-      eipAssociation
-      & eipaInstanceId ?~ Ref "EC2Instance"
-      & eipaEIP ?~ Ref "IPAddress"
-    ]
-    & description ?~ "See https://s3.amazonaws.com/cloudformation-templates-us-east-1/EIP_With_Association.template"
-    & formatVersion ?~ "2010-09-09"
-    & parameters ?~
-      [ parameter "InstanceType" "String"
-        & description ?~ "WebServer EC2 instance type"
-        & default' ?~ "t2.small"
-        & allowedValues ?~ [ "t1.micro", "t2.small" ]
-        & constraintDescription ?~ "must be a valid EC2 instance type."
-      , parameter "KeyName" "AWS::EC2::KeyPair::KeyName"
-        & description ?~ "Name of an existing EC2 KeyPair to enable SSH access to the instances"
-        & constraintDescription ?~ "must be the name of an existing EC2 KeyPair."
-      , parameter "IPAddress" "String"
-        & description ?~ "The IP address range that can be used to SSH to the EC2 instances"
-        & minLength ?~ 9
-        & maxLength ?~ 18
-        & default' ?~ "0.0.0.0/0"
-        & allowedValues ?~ [ "t1.micro", "t2.small" ]
-        & allowedPattern ?~ "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})/(\\d{1,2})"
-        & constraintDescription ?~ "must be a valid IP CIDR range of the form x.x.x.x/x."
+  [ resource "EC2Instance" (
+      EC2InstanceProperties $
+      ec2Instance
+      "ami-22111148"
+      & eciInstanceType ?~ Ref "InstanceType"
+      & eciKeyName ?~ Ref "KeyName"
+      & eciUserData ?~ Base64 (Join "" ["IPAddress=", Ref "IPAddress"])
+      & eciSecurityGroups ?~ [Ref "InstanceSecuritygroup"]
+      )
+    & deletionPolicy ?~ Retain
+  , resource "InstanceSecurityGroup" $
+    SecurityGroupProperties $
+    securityGroup
+    "Enable SSH Access"
+    & sgSecurityGroupIngress ?~ [
+      securityGroupIngressRule
+      "tcp"
+      & sgirFromPort ?~ Literal 22
+      & sgirToPort ?~ Literal 22
+      & sgirCidrIp ?~ Ref "SSHLocation"
       ]
-      & outputs ?~
-      [ output "InstanceId"
-        (Ref "EC2Instance")
-        & description ?~ "InstanceId of the newly created EC2 instance"
-      , output "InstanceIPAddress"
-        (Ref "IPAddress")
-        & description ?~ "IP address of the newly created EC2 instance"
-      ]
+  , resource "IPAddress" (EIPProperties eip)
+  , resource "IPAssoc" $
+    EIPAssociationProperties $
+    eipAssociation
+    & eipaInstanceId ?~ Ref "EC2Instance"
+    & eipaEIP ?~ Ref "IPAddress"
+  ]
+  & description ?~ "See https://s3.amazonaws.com/cloudformation-templates-us-east-1/EIP_With_Association.template"
+  & formatVersion ?~ "2010-09-09"
+  & parameters ?~
+  [ parameter "InstanceType" "String"
+    & description ?~ "WebServer EC2 instance type"
+    & default' ?~ "t2.small"
+    & allowedValues ?~ [ "t1.micro", "t2.small" ]
+    & constraintDescription ?~ "must be a valid EC2 instance type."
+  , parameter "KeyName" "AWS::EC2::KeyPair::KeyName"
+    & description ?~ "Name of an existing EC2 KeyPair to enable SSH access to the instances"
+    & constraintDescription ?~ "must be the name of an existing EC2 KeyPair."
+  , parameter "IPAddress" "String"
+    & description ?~ "The IP address range that can be used to SSH to the EC2 instances"
+    & minLength ?~ 9
+    & maxLength ?~ 18
+    & default' ?~ "0.0.0.0/0"
+    & allowedValues ?~ [ "t1.micro", "t2.small" ]
+    & allowedPattern ?~ "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})/(\\d{1,2})"
+    & constraintDescription ?~ "must be a valid IP CIDR range of the form x.x.x.x/x."
+  ]
+  & outputs ?~
+  [ output "InstanceId"
+    (Ref "EC2Instance")
+    & description ?~ "InstanceId of the newly created EC2 instance"
+  , output "InstanceIPAddress"
+    (Ref "IPAddress")
+    & description ?~ "IP address of the newly created EC2 instance"
+  ]
 
 main :: IO ()
 main = B.putStrLn $ encodeTemplate myTemplate
