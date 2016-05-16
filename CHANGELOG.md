2d7db034b6182771f30aef57ce6f90fbaba530b9
David Reaver <johndreaver@gmail.com>
2016-05-16 14:22:17 -0700

    Fix style test in cabal distribution

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.1.1
+
+* Small bug fix for "style" test when using the cabal distribution
+
 ## 0.1 (initial release)
 
 * Initial release with all Template components implemented along with a huge
--- tests/HLint.hs
+++ tests/HLint.hs
@@ -1,16 +1,21 @@
 module Main (main) where
 
 import Language.Haskell.HLint (hlint)
+import System.Directory (doesDirectoryExist)
 import System.Exit (exitFailure, exitSuccess)
 
 arguments :: [String]
 arguments =
     [ "library"
     , "tests"
-    , "gen"
     ]
 
 main :: IO ()
 main = do
-    hints <- hlint arguments
+    -- Need to check for existence of gen directory, because it is not present
+    -- in the cabal distribution.
+    genExists <- doesDirectoryExist "gen"
+    let args = if genExists then "gen" : arguments else arguments
+
+    hints <- hlint args
     if null hints then exitSuccess else exitFailure
