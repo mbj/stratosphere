b993f11b9b5bc6ead408f00572cfafbd0247b880
Alex Babkin <ababkin@gmail.com>
2016-10-03 07:39:04 -0400

    add custom execution role to the lambda example

--- examples/simple-lambda.hs
+++ examples/simple-lambda.hs
@@ -7,6 +7,7 @@ import Control.Lens
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Text (Text)
 import Stratosphere
+import Data.Aeson (object, Value(Array))
 
 main :: IO ()
 main = B.putStrLn $ encodeTemplate myTemplate
@@ -14,19 +15,21 @@ main = B.putStrLn $ encodeTemplate myTemplate
 myTemplate :: Template
 myTemplate =
   template
-  [ lambda ]
+  [ role, lambda ]
   & description ?~ "Lambda example"
   & formatVersion ?~ "2010-09-09"
 
 lambda :: Resource
 lambda =
-  resource "LambdaFunction" $
+  (resource "LambdaFunction" $
   LambdaFunctionProperties $
   lambdaFunction 
     lambdaCode 
     "index.handler" 
-    "arn:aws:iam::910653408535:role/MyLambdaBasicExecutionRole" 
+    (GetAtt "IAMRole" "Arn")
     "nodejs4.3"
+  )
+  & dependsOn ?~ [ Literal $ role ^. resName ]
 
 lambdaCode :: LambdaFunctionCode
 lambdaCode = lambdaFunctionCode 
@@ -40,3 +43,52 @@ code = "\
 \  callback(null, \"some success message\"); \
 \ } \
 \ "
+
+
+role :: Resource
+role = 
+  resource "IAMRole" $
+  IAMRoleProperties $
+  iamRole rolePolicyDocumentObject
+  & iamrPolicies ?~ [ executePolicy ]
+  & iamrRoleName ?~ "MyLambdaBasicExecutionRole"
+  & iamrPath ?~ "/"
+
+  where
+    executePolicy = 
+      iamPolicies
+      [ ("Version", "2012-10-17")
+      , ("Statement", statement) 
+      ] $
+      "MyLambdaExecutionPolicy"
+      
+      where
+        statement = object
+          [ ("Effect", "Allow")
+          , ("Action", actions)
+          , ("Resource", "*")
+          ]
+
+        actions = Array
+          [ "logs:CreateLogGroup"
+          , "logs:CreateLogStream"
+          , "logs:PutLogEvents"
+          ]
+      
+
+    rolePolicyDocumentObject =
+      [ ("Version", "2012-10-17")
+      , ("Statement", statement) 
+      ]
+
+      where
+        statement = object
+          [ ("Effect", "Allow")
+          , ("Principal", principal)
+          , ("Action", "sts:AssumeRole")
+          ]
+
+        principal = object
+          [ ("Service", "lambda.amazonaws.com") ]
+
+
